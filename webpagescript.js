9ccc7868c9fe0087225a2c03272843738b1cb5b0
raaidmustafa <raaidmustafa159@gmail.com>
2025-07-16 15:59:32 +0500

    wihe commit

--- webpagescript.js
+++ webpagescript.js
@@ -1,14 +1,13 @@
-let fullname = prompt("hello there! What is your fullname?");
-if (fullname === "raaid mustafa") {
-    let a = prompt("Welcome Raaid Mustafa! How was your day?").toLowerCase();
+let fullname = prompt("Hello there! What is your fullname?");
+if (fullname && fullname.toLowerCase() === "raaid mustafa") {
+    let a = prompt("Welcome Raaid Mustafa! How was your day?");
     if (a === "good") {
-        prompt("That's great to hear! But first I request you to enter the password in order to verify your identity.");
+        prompt("That's great to hear! First you are required to enter the password in order to verify your identity.");
     } else if (a === "bad") {
         prompt("I'm sorry to hear that. How can I help make it better?");
-        prompt("Ofcourse, I can help you with that. But before I do that, it is necessary for you to enter the password in order to verify your identity.");
+        prompt("Of course, I can help you with that. Before I do that, it is necessary for you to enter the password in order to verify your identity.");
     }
 } else {
     alert("You are not Raaid Mustafa, therefore you will not be allowed to enter this page.");
-    alert("Have a good day!");
+    alert("PARRE HO!");
 }
-  
